e5cf571d2f71473cf026a58646133e57695e56f0
j3-hardwick <james3.hardwick@live.uwe.ac.uk>
2022-05-16 19:44:27 +0100

    Initial Commit

---
+++ JustNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustNews
+//
+//  Created by James Hardwick on 16/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JustNews/JustNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNewsApp.swift
+//  JustNews
+//
+//  Created by James Hardwick on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct JustNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
